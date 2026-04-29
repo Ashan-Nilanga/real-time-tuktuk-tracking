fcb936434870aa01ec4f3b7226fea8670c81acd4
Ashan-Nilanga <101865264+Ashan-Nilanga@users.noreply.github.com>
2026-04-29 20:51:44 +0530

    live db connection try

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ const { Pool } = pkg;
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    options: '-c search_path=tuktuk'
   }
 });
