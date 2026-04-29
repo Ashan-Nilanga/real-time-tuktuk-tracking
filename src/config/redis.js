630adeb0d3955886580a4e3ac511f28f7959765c
Ashan-Nilanga <101865264+Ashan-Nilanga@users.noreply.github.com>
2026-04-29 19:29:49 +0530

    connected redis with vercel

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,23 @@
-import Redis from 'ioredis';
-import dotenv from 'dotenv';
-dotenv.config();
+// import Redis from 'ioredis';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
+// export const redis = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT
+// });
+
+import { createClient } from 'redis';
+
+export const redis = createClient({
+    username: 'default',
+    password: 'EhitLrOdzQHSxQVjiZqb8WGj0GVE4JYD',
+    socket: {
+        host: 'redis-13930.c73.us-east-1-2.ec2.cloud.redislabs.com',
+        port: 13930
+    }
 });
+
+redis.on('error', err => console.log('Redis Client Error', err));
+
+await redis.connect();
